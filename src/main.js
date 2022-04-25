7b9e90e5e71b1cbaef0915747fa9067f04c753b6
SebinSong <songxevin@gmail.com>
2022-04-25 16:52:49 +1200

    inital-commit: change the folder structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './views/App.vue'
 
 Vue.config.productionTip = false
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error(`uncaught Vue error in ${info}: `, err)
+}
 
 new Vue({
   render: h => h(App),
